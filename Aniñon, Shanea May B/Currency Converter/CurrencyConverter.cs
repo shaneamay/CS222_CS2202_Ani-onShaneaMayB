81fcab9914cc4da04d11fbc2af7ad31648ed836e
shaneamay <24-08103@g.batstate-u.edu.ph>
2026-02-09 10:14:29 +0800

    Add files via upload

---
+++ Aniñon, Shanea May B/Currency Converter/CurrencyConverter.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+Console.Write("Enter amount in USD: ");
+double usd = double.Parse(Console.ReadLine());
+
+Console.Write("Enter exchange rate from USD to EUR: ");
+double rate = double.Parse(Console.ReadLine());
+
+double eur = usd * rate;
+
+Console.WriteLine("Amount in EUR: {0:F2}", eur);
---
+++ Aniñon, Shanea May B/Interest Calculator/InterestCalculator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+Console.Write("Enter principal amount: ");
+double principal = double.Parse(Console.ReadLine());
+
+Console.Write("Enter rate of interest: ");
+double rate = double.Parse(Console.ReadLine());
+
+Console.Write("Enter time in years: ");
+int time = int.Parse(Console.ReadLine());
+
+double simpleInterest = (principal * rate * time) / 100;
+
+Console.WriteLine("Simple Interest: {0:F2}", simpleInterest);
