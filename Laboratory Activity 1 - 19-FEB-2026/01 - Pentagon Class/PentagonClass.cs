6fa686605f4bcb31633d561732fdfcd7760b459e
shaneamay <24-08103@g.batstate-u.edu.ph>
2026-02-19 08:45:29 +0800

    Add files via upload

---
+++ Laboratory Activity 1 - 19-FEB-2026/01 - Pentagon Class/PentagonClass.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+class Pentagon
+{
+    private double side;
+
+    public Pentagon()
+    {
+        side = 0;
+    }
+
+    public Pentagon(double sideLength)
+    {
+        side = sideLength;
+    }
+
+    public double GetPerimeter()
+    {
+        return 5 * side;
+    }
+
+    public double GetArea()
+    {
+        return (Math.Sqrt(5 * (5 + 2 * Math.Sqrt(5))) * side * side) / 4;
+    }
+
+    public void Display()
+    {
+        Console.WriteLine($"Side length: {side}");
+        Console.WriteLine($"Perimeter: {GetPerimeter():F3}");
+        Console.WriteLine($"Area: {GetArea():F3}");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Pentagon p1");
+        Console.WriteLine("------------------------");
+        Pentagon p1 = new Pentagon();
+        p1.Display();
+
+        Console.WriteLine();
+
+        Console.WriteLine("Pentagon p2");
+        Console.WriteLine("------------------------");
+        Pentagon p2 = new Pentagon(5);
+        p2.Display();
+    }
+}
